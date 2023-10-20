86ecf8c2884dac6a93c673b2f786342b0a28aa49
C0123G1-NguyenDucTrung <124018669+DucTrungC0123G1@users.noreply.github.com>
2023-10-20 15:34:09 +0700

    Bai tap s4 & fixed s3

---
+++ bai-tap/s3-2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import TodoApp from "./components/TodoApp";
+
+function App() {
+  return (
+    <div>
+      <TodoApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ bai-tap/s4/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Increase from "./components/Increase";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <Increase/>
+    </div>
+  );
+}
+
+export default App;
