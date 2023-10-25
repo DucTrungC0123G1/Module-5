c574064ee5e63a3693de9a9afa1160c3a11b5104
C0123G1-NguyenDucTrung <124018669+DucTrungC0123G1@users.noreply.github.com>
2023-10-25 20:47:35 +0700

    update s7

--- bai-tap/s7-1/src/App.js
+++ bai-tap/s7-1/src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import {UserList} from "./components/UserList";
+
 import {Route, Routes} from "react-router-dom";
+import UserList from "./components/UserList";
 
 function App() {
   return (
